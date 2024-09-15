bd0c6c0b385c4f4e2a9f197ff99d5b4730b1c2b0
Dea7h <emil-n1k@hotmail.com>
2024-09-15 13:56:55 +0300

    init

---
+++ src/store/FiltersContext.ts
@@ -0,0 +1,11 @@
+import { createContext } from 'react'
+
+interface FiltersContext {
+  search: string
+  setSearch: React.Dispatch<React.SetStateAction<string>>
+}
+
+export const FiltersContext = createContext<FiltersContext>({
+  search: '',
+  setSearch: () => {},
+})
